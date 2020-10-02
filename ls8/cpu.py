042060ed0408d02fe4e6eac5a00febc0530afcc7
garybot <argot.pryer@gmail.com>
2020-10-01 17:46:10 -0700

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,16 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.reg[7] = 0xF4
+
+        self.pc = 0
+        self.IR = 0
+        self.MAR = 0
+        self.MDR = 0
+        self.fl = 0b00000000
+
+        self.ram = [0] * 256
 
     def load(self):
         """Load a program into memory."""
@@ -48,7 +57,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
+            # self.fl,
             #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -60,6 +69,32 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        while running:
+            command = self.ram[self.pc]
+            self.trace()
+            if command == 0b10000010: # LDI
+                self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2)
+                self.pc += 2
+            
+            elif command == 0b01000111: # PRN
+                print(self.reg[self.ram_read(self.pc + 1)])
+                self.pc += 1
+            
+            elif command == 0b00000001: # HLT
+                running = False
+            
+            else:
+                print('unknown command!')
+                running = False
+            
+            self.pc += 1
